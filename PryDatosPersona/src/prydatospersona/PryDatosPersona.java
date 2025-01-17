4e20a55e716459e43105cb6b860cce7f0f286353
DylanRodriguez19 <dylanrodriguez102005@gmail.com>
2025-01-17 11:11:12 -0500

    Add files via upload

---
+++ PryDatosPersona/src/prydatospersona/PryDatosPersona.java
@@ -0,0 +1,12 @@
+package prydatospersona;
+
+import VistaDatos.DatosPersonaVista;
+
+public class PryDatosPersona {
+    public static void main(String[] args) {
+            DatosPersonaVista datos = new DatosPersonaVista();
+            datos.setVisible(true);
+            datos.setLocationRelativeTo(null);
+   }
+    
+}
