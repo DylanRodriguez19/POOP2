1d162aa01dbe2e6772067d5d770712e1987e379e
DylanRodriguez19 <dylanrodriguez102005@gmail.com>
2025-01-20 10:55:57 -0500

    Add files via upload

---
+++ PryNavegacion/src/prynavegacion/PryNavegacion.java
@@ -0,0 +1,12 @@
+package prynavegacion;
+
+import Vista.PantallaPrincipal;
+
+public class PryNavegacion {
+    public static void main(String[] args) {
+        PantallaPrincipal paprincipal = new PantallaPrincipal();
+        paprincipal.setVisible(true);
+        paprincipal.setLocationRelativeTo(null);
+    }
+    
+}
